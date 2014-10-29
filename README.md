0fd47d148565a3440119a7b022f5bde7d719c11a
David Hornick <dave@fluidthinking.com>
2014-10-29 11:52:57 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+async-loader
+------------
+
+This is a simple AMD script for loading scripts and stylesheets asynchronously.
+
+### Tools Utilized
+
+ -  [Grunt](http://gruntjs.com/)
+ -  [Bower](http://bower.io/)
+
+See package.json and bower.json for individual libraries and frameworks.
+
+### Notes
+
+__Github__: There are debates as to whether you should include the packages from bower/grunt in the repository...  It is possible for them to disappear and then no longer be able to be installed.  I prefer not to clutter the repository and run the risk of packages being missing.  If a package is removed by its maintainer, do you really want to continue using it?
---
+++ dev/asyncLoader.js
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ *
+ * Async Loader
+ *
+ * Loads various scripts into the document.  Appends them after a defined node.
+ *
+*******************************************************************************/
+define([], function() {
+
+function insertElement( element, node ) {
+	node.parentNode.insertBefore( element, node );
+}
+
+function createScript( src ) {
+	var s = document.createElement( 'script' );
+	s.type = 'text/javascript';
+	s.async = true;
+	s.src = src;
+	return s;
+}
+
+return {
+	facebook: function( fbPixelId, node ) {
+		var _fbq = window._fbq = window._fbq || [];
+		if (!_fbq.loaded) {
+			insertElement( createScript( '//connect.facebook.net/en_US/fbds.js' ), node );
+			_fbq.loaded = true;
+		}
+		_fbq.push(['addPixelId', fbPixelId]);
+		_fbq.push(['track', 'PixelInitialized', {}]);
+	},
+	googleAnalytics: function( accountId, node ) {
+		var _gaq = window._gaq = window._gaq || [];
+		_gaq.push(['_setAccount', accountId]);
+		_gaq.push(['_trackPageview']);
+		insertElement( createScript( ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js' ), node );
+	},
+	stylesheet: function( href, node ) {
+		var link = document.createElement( "link" );
+		link.rel = "stylesheet";
+		link.href = href;
+		insertElement( link, node );
+	}
+};
+
+});
